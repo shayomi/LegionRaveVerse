1881b68cfd6726b6f6f7d27dda075bcda5b50694
shayomi <steve.legion94@gmail.com>
2023-09-04 15:51:24 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: "world-1",
+    imgUrl: "/explore1.JPG",
+    title: "Laughters  and Sorrows",
+  },
+  {
+    id: "world-2",
+    imgUrl: "/explore2.JPG",
+    title: "Beyonce-Lemonade",
+  },
+  {
+    id: "world-3",
+    imgUrl: "/explore3.JPG",
+    title: "Drake-More Life",
+  },
+  {
+    id: "world-4",
+    imgUrl: "/explore4.JPG",
+    title: "Drake- Livelong",
+  },
+  {
+    id: "world-5",
+    imgUrl: "/explore5.JPG",
+    title: "Jcole- Forest hill dreams",
+  },
+];
+
+export const startingFeatures = [
+  "Find an artist world you want to rave to",
+  "Check your VR devices",
+  "No need to beat around the bush, enjoy the rave",
+];
+
+export const newFeatures = [
+  {
+    imgUrl: "/vrpano.svg",
+    title: "A new Rave World",
+    subtitle:
+      "we have the latest albums with new rave for you to try and enjoy",
+  },
+  {
+    imgUrl: "/headset.svg",
+    title: "More realistic",
+    subtitle:
+      "In the latest update, your eyes are narrow, making the world more realistic than ever",
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: "/blog1.JPG",
+    title: "The launch of the LegionRave has increased music tech activities",
+    subtitle:
+      "Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.",
+  },
+  {
+    imgUrl: "/blog2.JPG",
+    title: "Learning the new activities of the RaveTechnology",
+    subtitle:
+      "Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum",
+  },
+  {
+    imgUrl: "/blog3.JPG",
+    title: "The emergence of Anonymous Dj and technical beat makers",
+    subtitle:
+      "Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem",
+  },
+];
+
+export const socials = [
+  {
+    name: "twitter",
+    url: "/twitter.svg",
+  },
+  {
+    name: "linkedin",
+    url: "/linkedin.svg",
+  },
+  {
+    name: "instagram",
+    url: "/instagram.svg",
+  },
+  {
+    name: "facebook",
+    url: "/facebook.svg",
+  },
+];
